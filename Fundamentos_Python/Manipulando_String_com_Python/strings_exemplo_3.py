f204a236a43c8d49fdcb415f09326472a03ed825
Thiago <tlucenalive@gmail.com>
2024-08-27 08:54:04 -0300

    Fundamentos Python

---
+++ Fundamentos_Python/Manipulando_String_com_Python/strings_exemplo_3.py
@@ -0,0 +1,11 @@
+#FATIAMENTO DE STRINGS 
+
+nome = "Carlos Eduardo da Silva"
+
+print(nome [0])
+print(nome [:9])
+print(nome [15:])
+print(nome[7:14])
+print(nome [7:14:2])
+print(nome [:])
+print(nome [::-1])
---
+++ Fundamentos_Python/Manipulando_String_com_Python/strings_exemplo_4.py
@@ -0,0 +1,18 @@
+# UTILIZAÇÃO DE """ OU ''' PARA FORMATAÇÃO DE TEXTOS DE VIZUALIZAÇÃO
+
+nome = "Josue"
+
+mensagem = f"""Olá meu nome é {nome} 
+                prazer em conhecer.
+        Você vem sempre aqui?    """
+
+print(mensagem)
+
+print(""" 
+    ********* Menu ********* 
+      1 - Sacar
+      2 - Depositar
+      3 - Sair
+
+********* Volte Sempre ********* 
+""")
